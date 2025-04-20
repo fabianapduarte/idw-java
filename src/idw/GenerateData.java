0dd8b6534740292eb93da880b5cca5105efa56ac
Fabiana Pereira <fabianaduarte980@gmail.com>
2025-04-20 18:30:51 -0300

    feat: add serial algorithm and class to generate data

---
+++ src/idw/GenerateData.java
@@ -0,0 +1,35 @@
+package idw;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Random;
+
+public class GenerateData {
+	private static final Path FILE = Path.of("./data/measurements.txt");
+
+	public static void main(String[] args) throws IOException {
+		final int SIZE = 100_000_000;
+		Random rand = new Random();
+
+    System.out.println("Gerando arquivo...");
+
+		try (BufferedWriter bw = Files.newBufferedWriter(FILE)) {
+			for (int i = 0; i < SIZE; i++) {
+				int x = rand.nextInt(10_000);
+				int y = rand.nextInt(10_000);
+				double temp = 30 - 0.005 * y + 0.003 * x + 5 * Math.sin(x / 500.0) + 3 * Math.cos(y / 500.0)
+						+ (rand.nextDouble() * 2 - 1);
+				bw.write(x + "," + y + "," + String.format("%.1f", temp));
+				bw.newLine();
+
+				if (i % 10_000_000 == 0) {
+					System.out.println("Gerado: " + i);
+				}
+			}
+		}
+
+		System.out.println("Arquivo gerado com sucesso.");
+	}
+}
---
+++ src/idw/IDWInterpolationSerial.java
@@ -0,0 +1,61 @@
+package idw;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class IDWInterpolationSerial {
+	private static final String FILE = "./data/measurements.txt";
+
+	public record Point(int x, int y, double value) {
+	}
+
+	public static double calculateInverseDistanceWeighting(Point[] mapPoints, int power, Point point) {
+		double numerator = 0.0;
+		double weights = 0.0;
+
+		for (Point mapPoint : mapPoints) {
+			double distance = getDistance(mapPoint, point);
+
+			if (distance == 0.0) {
+				return mapPoint.value();
+			}
+
+			double weight = 1.0 / Math.pow(distance, power);
+			numerator += mapPoint.value() * weight;
+			weights += weight;
+		}
+
+		return numerator / weights;
+	}
+
+	public static double getDistance(Point p, Point q) {
+		return Math.hypot(p.x() - q.x(), p.y() - q.y());
+	}
+
+	public static void main(String[] args) throws IOException {
+		int x = 0, y = 0;
+
+		try {
+			x = Integer.parseInt(args[0]);
+			y = Integer.parseInt(args[1]);
+		} catch (NumberFormatException e) {
+			System.out.println("Coordenadas inválidas.");
+			System.exit(1);
+		}
+
+		Point[] mapPoints = Files.lines(Paths.get(FILE)).map(line -> {
+			String[] data = line.split(",");
+			int lineX = Integer.parseInt(data[0]);
+			int lineY = Integer.parseInt(data[1]);
+			double lineValue = Double.parseDouble(data[2]);
+			return new Point(lineX, lineY, lineValue);
+		}).toArray(Point[]::new);
+
+		Point point = new Point(x, y, -1);
+
+		double idw = calculateInverseDistanceWeighting(mapPoints, 2, point);
+
+		System.out.println("IDW: " + idw);
+	}
+}
